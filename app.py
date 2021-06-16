042e780ab9a2ef6121ed1683bc41827589ce2a33
Arian Mirahmadi <mirarianmir@gmail.com>
2021-06-17 01:18:57 +0430

    feat: trim webpage content

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask
 from flask import request
 import trim
+import get
 
 app = Flask(__name__)
 
-@app.route('/trim')
-def route_trim():
-    text = request.args.get('text')
-    trimed = trim.trim_text(text)
-    # convert list to dictionary
+def convert_to_dict(converting_list):
     litexes = {}
-    for i, litex in enumerate(trimed):
+    for i, litex in enumerate(converting_list):
         litexes[i] = litex
     return litexes
+
+@app.route('/trim/text')
+def route_trim_text():
+    text = request.args.get('text')
+    trimed = trim.trim_text(text)
+    return convert_to_dict(trimed)
+
+@app.route('/trim/url')
+def route_trim_url():
+    url = request.args.get('url')
+    print(url)
+    text = get.get_text_of_webpage(url)
+    print(text[0:100])
+    trimed = trim.trim_text(text)
+    print(trimed)
+    return convert_to_dict(trimed)
--- get.py
+++ get.py
@@ -1,6 +1,5 @@
 from urllib import request
 import re
-from os import system
 
 def clean_html(raw_html):
     cleaner = re.compile('<.*?>')
@@ -13,10 +12,6 @@ def get_paragraphs(raw_html):
         paragraphs += clean_html(p)
     return paragraphs.replace('\\n', '\n').replace('\\t', '\t'.replace('\\s', '\s'))
 
-url = input("URL: ")
-html = request.urlopen(url).read()
-
-paragraphs = get_paragraphs(html)
-open("file.txt","wt").write(paragraphs)
-
-system("py trim.py")
+def get_text_of_webpage(url):
+    html = request.urlopen(url).read()
+    return get_paragraphs(html)
